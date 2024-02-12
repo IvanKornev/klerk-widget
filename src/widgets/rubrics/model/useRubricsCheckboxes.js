0c73d1503f976de4b6cd6f3ae7aeb6fb5329ae2d
IvanKornev <brovkin.komar@yandex.ru>
2024-02-12 19:07:00 +0300

    refactor: вынес код тоггла чекбоксов подрубрик вне миксина

--- src/widgets/rubrics/model/useRubricsCheckboxes.js
+++ src/widgets/rubrics/model/useRubricsCheckboxes.js
@@ -1,3 +1,22 @@
+function _addSubrubric(payload = {}) {
+  const { rubricId, subrubricId } = payload;
+  if (!this.checkedRubrics[rubricId]) {
+    this.checkedRubrics[rubricId] = [];
+  }
+  this.checkedRubrics[rubricId].push(subrubricId);
+};
+
+function _removeSubrubric(payload = {}) {
+  const { rubricId, subrubricId } = payload;
+  const filteredRubrics = this.checkedRubrics[rubricId].filter(
+    (currentId) => subrubricId !== currentId,
+  );
+  this.checkedRubrics[rubricId] = filteredRubrics;
+  if (this.checkedRubrics[rubricId].length === 0) {
+    delete this.checkedRubrics[rubricId];
+  }
+};
+
 const useRubricsCheckboxes = {
   data() {
     return {
@@ -6,29 +25,12 @@ const useRubricsCheckboxes = {
   },
   methods: {
     changeCheckedRubrics(payload, actionName) {
-      const possibleActions = {
-        'subrubric-adding': (payload) => this.addSubrubric(payload),
-        'subrubric-removing': (payload) => this.removeSubrubric(payload),
+      const POSSIBLE_ACTIONS = {
+        'subrubric-adding': _addSubrubric,
+        'subrubric-removing': _removeSubrubric,
       };
-      const actionCallback = possibleActions[actionName];
-      actionCallback(payload);
-    },
-    addSubrubric(payload = {}) {
-      const { rubricId, subrubricId } = payload;
-      if (!this.checkedRubrics[rubricId]) {
-        this.checkedRubrics[rubricId] = [];
-      }
-      this.checkedRubrics[rubricId].push(subrubricId);
-    },
-    removeSubrubric(payload = {}) {
-      const { rubricId, subrubricId } = payload;
-      const filteredRubrics = this.checkedRubrics[rubricId].filter(
-        (currentId) => subrubricId !== currentId,
-      );
-      this.checkedRubrics[rubricId] = filteredRubrics;
-      if (this.checkedRubrics[rubricId].length === 0) {
-        delete this.checkedRubrics[rubricId];
-      }
+      const actionCallback = POSSIBLE_ACTIONS[actionName];
+      actionCallback.bind(this)(payload);
     },
   },
 };
