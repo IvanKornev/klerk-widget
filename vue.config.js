f325dd02ed54427ddcd01874268d201eef952ade
IvanKornev <brovkin.komar@yandex.ru>
2024-02-13 10:31:29 +0300

    build: host-параметр в конфиге vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   devServer: {
-    disableHostCheck: true,
+    host: '0.0.0.0',
   },
   transpileDependencies: true
 });
