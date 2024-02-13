02813139045d3f4d78f2dd659852a06e3f2c2f3e
IvanKornev <brovkin.komar@yandex.ru>
2024-02-13 10:35:36 +0300

    build: allowedHosts в конфиге

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   devServer: {
-    host: '0.0.0.0',
+    allowedHosts: 'all',
   },
   transpileDependencies: true
 });
