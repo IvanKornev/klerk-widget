82cf1faee329010d3b04c9fb8fdee72a21857866
IvanKornev <brovkin.komar@yandex.ru>
2024-06-22 21:12:41 +0300

    refactor: переписал SumTitle.vue на TS

--- src/entities/rubric/ui/SumTitle.vue
+++ src/entities/rubric/ui/SumTitle.vue
@@ -4,23 +4,15 @@
   </h1>
 </template>
 
-<script>
-export default {
-  props: {
-    checkedRubrics: {
-      type: Object,
-      default() {
-        return {};
-      },
-    },
-  },
-  computed: {
-    allCountsSum() {
-      const onlyCounts = Object.values(this.checkedRubrics);
-      return onlyCounts.reduce((acc, count) => (
-        acc += count
-      ), 0);
-    },
-  },
-};
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps<{
+  checkedRubrics: Record<number, number>,
+}>();
+
+const allCountsSum = computed(() => {
+  const onlyCounts = Object.values(props.checkedRubrics);
+  return onlyCounts.reduce((acc, count) => acc += count, 0);
+});
 </script>
